1341dafd5c26b374f1bc4c489da123351bf7edee
Kingaa6 <Kingaa6@gmail.com>
2019-05-06 16:36:35 -0700

    Add linked list solution to queue question

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,68 +1,115 @@
 """Each ListNode holds a reference to its previous node
 as well as its next node in the List."""
-class ListNode:
-  def __init__(self, value, prev=None, next=None):
-    self.value = value
-    self.prev = prev
-    self.next = next
 
-  """Wrap the given value in a ListNode and insert it
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    """Wrap the given value in a ListNode and insert it
   after this node. Note that this node could already
   have a next node it is point to."""
-  def insert_after(self, value):
-    current_next = self.next
-    self.next = ListNode(value, self, current_next)
-    if current_next:
-      current_next.prev = self.next
 
-  """Wrap the given value in a ListNode and insert it
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
   before this node. Note that this node could already
   have a previous node it is point to."""
-  def insert_before(self, value):
-    current_prev = self.prev
-    self.prev = ListNode(value, current_prev, self)
-    if current_prev:
-      current_prev.next = self.prev
 
-  """Rearranges this ListNode's previous and next pointers
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
   accordingly, effectively deleting this ListNode."""
-  def delete(self):
-    if self.prev:
-      self.prev.next = self.next
-    if self.next:
-      self.next.prev = self.prev
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
 
 """Our doubly-linked list class. It holds references to
 the list's head and tail nodes."""
+
+
 class DoublyLinkedList:
-  def __init__(self, node=None):
-    self.head = node
-    self.tail = node
-    self.length = 1 if node is not None else 0
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
 
-  def __len__(self):
-    return self.length
+    def __len__(self):
+        return self.length
 
-  def add_to_head(self, value):
-    pass
+    def add_to_head(self, value):
+        new_node = ListNode(value)
 
-  def remove_from_head(self):
-    pass
+        # ll is empty - assign new node to both head and tail
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
 
-  def add_to_tail(self, value):
-    pass
+        # ll has more than one node
+        else:
+            self.head.insert_before(value)
+            self.head.prev = None
+            self.length += 1
 
-  def remove_from_tail(self):
-    pass
+    def remove_from_head(self):
+        # if ll has head, update head to none, update new head prev to none
+        pass
 
-  def move_to_front(self, node):
-    pass
+    def add_to_tail(self, value):
+        pass
 
-  def move_to_end(self, node):
-    pass
+    def remove_from_tail(self):
+        pass
 
-  def delete(self, node):
-    pass
-    
-  def get_max(self):
-    pass
+    def move_to_front(self, node):
+        pass
+
+    def move_to_end(self, node):
+        pass
+
+    def delete(self, node):
+        pass
+
+    def get_max(self):
+        pass
+
+    def display(self):
+        display = []
+        current_node = self.head
+
+        while current_node.next != None:
+            current_node = current_node.next
+            display.append(current_node.value)
+        return display
+
+
+ll = DoublyLinkedList()
+
+another_node = ListNode(4)  # None, 4, None
+# print(new_node.prev)
+
+ll.add_to_head(4)
+print("previous:", ll.head.prev, "value:",
+      ll.head.value, "next:", ll.head.next)
+print("tail", ll.tail.prev, ll.tail.value, ll.tail.next)
+
+ll.add_to_head(6)
+print("head", ll.head.prev, ll.head.value, ll.head.next)
+print("tail", ll.tail.prev, ll.tail.value, ll.tail.next)
+
+print(ll.display())
--- queue/queue.py
+++ queue/queue.py
@@ -1,51 +1,36 @@
 
-class Queue:
-    def __init__(self):
-        self.size = 0
-        # what data structure should we
-        # use to store queue elements?
-        self.storage = []
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         # what data structure should we
+#         # use to store queue elements?
+#         self.storage = []
 
-    def enqueue(self, item):
-        self.size += 1
-        return self.storage.insert(0, item)
+#     def enqueue(self, item):
+#         self.size += 1
+#         return self.storage.insert(0, item)
 
-    def dequeue(self):
-        if self.size > 0:
-            self.size -= 1
-            return self.storage.pop(len(self.storage) - 1)
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop(len(self.storage) - 1)
 
-    def len(self):
-        if self.size > 0:
-            return len(self.storage)
-        else:
-            return 0
+#     def len(self):
+#         if self.size > 0:
+#             return len(self.storage)
+#         else:
+#             return 0
 
-# To optimize for inserting and deleting, a linked list could be used
-
-
-test = Queue()
-print(test.storage)
-
-test.enqueue(1)
-
-print(test.storage)
-
-# test.dequeue()
-
-# print(test.storage)
-
-
-print(test.len())
+# # To optimize for inserting and deleting, a linked list could be used
 
 
 class Node:
-    def __init__(self, value, next_node=None):
-        self.value = value
+    def __init__(self, name, next_node=None):
+        self.name = name
         self.next_node = next_node
 
-    def get_value(self):
-        return self.value
+    def get_name(self):
+        return self.name
 
     def get_next(self):
         return self.next_node
@@ -54,11 +39,9 @@ class Node:
         self.next_node = new_next
 
 # Queue would be a linked list
-# Back of line (head)
-# Front of line (tail)
 
 
-class QueueLL:
+class Queue:
     def __init__(self):
         self.size = 0
         # what data structure should we
@@ -67,31 +50,40 @@ class QueueLL:
         self.head = None
         self.tail = None
 
-    def enqueue(self, item):
+    def enqueue(self, node):
+        new_node = Node(node)
         # what if the queue is empty?
         if not self.head and not self.tail:
             # set incoming node to head and tail
-            self.head = Node(item)
-            self.tail = Node(item)
-        # what if the queue has one item?
-            # update head to point to new node
-            # point new node to tail
-        # what if the queue has multiple
-            # create a node, point it to old head
-        pass
+            self.head = new_node
+            self.tail = new_node
+        # what if the queue has nodes?
+        else:
+            # set current tail next to new node
+            self.tail.set_next(new_node)
+            # update tail with new node
+            self.tail = new_node
 
     def dequeue(self):
+        dequeue_node = self.head
         # what if queue is empty?
-            # return None
-        # what if queue has one item?
+        if not self.head and not self.tail:
+            return None
+        # what if queue has one node?
+        if self.head == self.tail:
             # remove reference from queue, head = None, tail = None
-        # what if queue has multiple items?
-            # iterate through items until you reach the end, remove the reference from the second to last node
-            # update tail to second to last node
-        pass
+            self.head = None
+            self.tail = None
+        # what if queue has multiple nodes?
+        else:
+            self.head = self.head.get_next()
+
+        return dequeue_node.get_name()
 
     def len(self):
-        # use size variable
-        # while node reference is not None
-            # increment size by 1
-        pass
+        count = 0
+        current_node = self.head
+        while current_node != None:
+            count += 1
+            current_node = current_node.get_next()
+        return count
