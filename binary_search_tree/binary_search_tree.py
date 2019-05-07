3a255c3aa2ad4d0100d91216cf8beb865c9d9f7f
Kingaa6 <Kingaa6@gmail.com>
2019-05-07 16:37:33 -0700

    Add binary search insert method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,31 @@
+
+# each node is it's own BST instance, it only knows about it's children nodes
+
+
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # create new node if conditions are met
+        if value <= self.value and not self.left:
+            self.left = BinarySearchTree(value)
+        elif value >= self.value and not self.right:
+            self.right = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+        # recursive logic
+        elif value <= self.value:
+            self.left.insert(value)
+        elif value >= self.value:
+            self.right.insert(value)
 
-  def get_max(self):
-    pass
+    def contains(self, target):
+        pass
 
-  def for_each(self, cb):
-    pass
+    def get_max(self):
+        pass
+
+    def for_each(self, cb):
+        pass
