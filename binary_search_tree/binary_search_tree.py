feec27200fb0a0b7f7f5a2bb6dcbb7fc6a61027e
Kingaa6 <Kingaa6@gmail.com>
2019-05-08 12:26:22 -0700

    Add for_each to binary search, passing all tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,26 +9,33 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value):
+        # assess left tree
         if value < self.value:
             if not self.left:
                 self.left = BinarySearchTree(value)
+            # run insert on left node if available
             else:
                 self.left.insert(value)
 
+        # assess right tree
         elif value > self.value:
             if not self.right:
                 self.right = BinarySearchTree(value)
+            # run insert on right node if available
             else:
                 self.right.insert(value)
 
     def contains(self, target):
         if target == self.value:
             return True
+        # check left node
         elif target < self.value and self.left:
             return self.left.contains(target)
+        # check right node
         elif target > self.value and self.right:
             return self.right.contains(target)
-        elif target != self.value:
+        # no match
+        else:
             return False
 
     def get_max(self):
@@ -41,4 +48,14 @@ class BinarySearchTree:
             return self.right.get_max()
 
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.left and self.right:
+            self.left.for_each(cb)
+            self.right.for_each(cb)
+
+        if self.left:
+            self.left.for_each(cb)
+
+        if self.right:
+            self.right.for_each(cb)
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,52 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        # remove priority element
+        # last element in array swaps with new open space
+        # now soft down new priority element
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        # get priority element and return it O(1)
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        # len(arr)?
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    # index parameter is the index of the node whereever it is in the array
+    def _bubble_up(self, index):
+        # loop until the element reaches the top of the array
+        # or we'll break the loop when we realize the element's priority is not larger than parent
+        while index > 0:
+            # the value at "index" fetches the index of its parent
+            parent = (index - 1) // 2
+            # check if the element at index has higher priority than the element at the parent index
+            if self.storage[index] > self.storage[parent]:
+                # swap elements
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                # we also need to update the index
+                index = parent
+            else:
+                # otherwise, our element has reached a spot in the heap where its parent element has higher priority, stop climbing
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        # left = 2 * index + 1
+        # right = 2 * index + 2
+        pass
+
+
+'''
+insert will stick the value at the end of the array
+bubble up will be used by insert to move the element to it's proper spot
+
+delete will delete the main root and moves a leaf to the main node
+sift down will move the element down to it's proper spot
+
+'''
